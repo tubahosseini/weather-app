cdde0b4ee6e2bcffb7c18a6645c3e5f394c02095
Tuba Hosseini <tubahosseini@gmail.com>
2024-06-02 02:20:20 +0330

    use searchParams for roating

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,11 +1,10 @@
 import { Paper, Box, Grid } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
 export default function Auth() {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
+  const [searchParams] = useSearchParams();
   const mode = searchParams.get("mode");
 
   return (
@@ -98,3 +97,104 @@ export default function Auth() {
     </Grid>
   );
 }
+
+// import { Paper, Box, Grid } from "@mui/material";
+// import { useLocation } from "react-router-dom";
+// import SignIn from "./SignIn";
+// import SignUp from "./SignUp";
+
+// export default function Auth() {
+//   const location = useLocation();
+//   const searchParams = new URLSearchParams(location.search);
+//   const mode = searchParams.get("mode");
+
+//   return (
+//     <Grid container component="main" sx={{ height: "100vh" }}>
+//       {mode === "signup" ? (
+//         <>
+//           <Grid
+//             item
+//             xs={12}
+//             sm={8}
+//             md={5}
+//             order={{ xs: 2, sm: 2, md: 2 }} // ensures this item comes second
+//             component={Paper}
+//             elevation={6}
+//             square
+//             sx={{
+//               backgroundColor: "#000000ab",
+//               color: "#fff",
+//             }}
+//           >
+//             <Box
+//               sx={{
+//                 my: 8,
+//                 mx: 4,
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 alignItems: "center",
+//               }}
+//             >
+//               <SignUp />
+//             </Box>
+//           </Grid>
+//           <Grid
+//             item
+//             xs={12}
+//             sm={4}
+//             md={7}
+//             order={{ xs: 1, sm: 1, md: 1 }} // ensures this item comes first
+//           >
+//             <div className="clouds">
+//               <div className="clouds-1"></div>
+//               <div className="clouds-2"></div>
+//               <div className="clouds-3"></div>
+//             </div>
+//           </Grid>
+//         </>
+//       ) : (
+//         <>
+//           <Grid
+//             item
+//             xs={12}
+//             sm={4}
+//             md={7}
+//             order={{ xs: 2, sm: 2, md: 2 }} // ensures this item comes first
+//           >
+//             <div className="clouds">
+//               <div className="clouds-1"></div>
+//               <div className="clouds-2"></div>
+//               <div className="clouds-3"></div>
+//             </div>
+//           </Grid>
+//           <Grid
+//             item
+//             xs={12}
+//             sm={8}
+//             md={5}
+//             order={{ xs: 1, sm: 1, md: 1 }} // ensures this item comes second
+//             component={Paper}
+//             elevation={6}
+//             square
+//             sx={{
+//               backgroundColor: "#000000ab",
+//               color: "#fff",
+//             }}
+//           >
+//             <Box
+//               sx={{
+//                 my: 8,
+//                 mx: 4,
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 alignItems: "center",
+//               }}
+//             >
+//               <SignIn />
+//             </Box>
+//           </Grid>
+//         </>
+//       )}
+//     </Grid>
+//   );
+// }
